503b8cb588a6920bdafe5853c9c4c1162bb6ef36
curiousTC <tcvang06@gmail.com>
2020-12-09 12:33:33 -0500

    Added day 9 solution

---
+++ Day9/day9.py
@@ -0,0 +1,48 @@
+def findInvalidXMASPart1(inputList, preamble):
+    for i in range(preamble, len(inputList)):
+        prev = list(inputList[i-preamble:i])
+
+        sumVal = 0
+        validSum = set()
+        target = int(inputList[i])
+        for j in range(len(prev)):
+            for k in range(len(prev)):
+                if prev[j] != prev[k]:
+                    sumVal = int(prev[j]) + int(prev[k])
+                    if sumVal == target:
+                        validSum.add(sumVal)
+                    elif sumVal != target:
+                        continue
+        
+        if not validSum:
+            return target
+
+def findEncryptionWeakness(inputList, target):
+    result = 0
+    for i in range(len(inputList)):
+        for j in range(i, len(inputList)):
+            subList = [int(v) for v in inputList[i:j]]
+
+            if sum(subList) == target: 
+                result = min(subList) + max(subList)
+
+                return result
+
+    return target
+                    
+def main():
+    inputList = []
+    with open('input.txt') as f:
+        for line in f:
+            inputList.append(line.replace("\n",""))
+    
+    #print(inputList)
+
+    res = findInvalidXMASPart1(inputList, 25)
+    print(f'Part 1 - Results: {res}')
+
+    res2 = findEncryptionWeakness(inputList, res)
+    print(f'Part 2 - Results: {res2}')
+
+if __name__ == "__main__":
+    main()
